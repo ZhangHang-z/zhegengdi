4ac730bf31c8c021fdcdf9284331c1017ca6facd
HangZhang <stevezhang2014@gmail.com>
2016-02-18 21:10:43 +0800

    add usersubmit

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,6 +6,7 @@
 
 var ejs = require('ejs');
 var fs = require('fs');
+var util = require('util');
 var mySys = require('../common_functions/mysys.js');
 
 var adminUserModel = require('../models/admin_user_model.js');
@@ -23,13 +24,27 @@ exports.login = function login(req, res) {
 
 
 exports.createUser = function createUser(req, res) {
-  var create_user_text = fs.readFileSync(mySys.getUserHome() + '/zhegengdi/views/admin/create_user.ejs', 'utf-8');
+  var create_user_text = fs.readFileSync(mySys.getUserHome() +
+      '/zhegengdi/views/admin/create_user.ejs', 'utf-8');
   var create_user_HTML = ejs.render(create_user_text);
   res.render('admin/admin_lay.ejs', { body: create_user_HTML });
 };
 
 
 exports.newUserSubmit = function newUserSubmit(req, res) {
-  var post = req.body;
-  console.log(post['account'])
+  var account = req.body['account'];
+  var password = req.body['password'];
+  var mobile = req.body['mobile'];
+  var name = req.body['name'];
+  var email = req.body['email'];
+  var nickname = req.body['nickname'];
+  var level = parseInt(req.body['level']);
+  if (!account || !password || !mobile || !level || !name) {
+    res.status('403').send('请输入完整的信息！');
+  }
+  if (isNaN(level)) {
+    res.status('403').send('请勿修改前端脚本，等级权限必须为数字！');
+  }
+
+
 };
